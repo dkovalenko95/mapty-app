a6db51f268496e2c18a13b75907b13faf216be3a
Dmytro <dkovalenko2425@gmail.com>
2022-07-07 12:38:14 +0300

    refactoring, added class App

--- script.js
+++ script.js
@@ -11,76 +11,88 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+// APPLICATION ARCHITECTURE
+class App {
+  // Private fields(private instance properties)
+  #map;
+  #mapEvent;
 
-// FUNCTIONALITY:
-// Get position(Geolocation API):
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    // Success
-    function(position) {
-      const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.com.ua/maps/@${latitude},${longitude},13z`);
+  constructor() {
+    this._getPosition();
+    // Form submit
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    // Toggle 'input' type
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      const coords = [latitude, longitude];
-      const staticCoords = [50.3671577, 30.4406045];
-
-      // Define 'mapE' as a Global variable
-      map = L.map('map').setView(coords, 13);
-      console.log(map);
-
-      // Google Map tiles
-      L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 18,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      }).addTo(map);
-
-      // Handling Clicks on Map
-      map.on('click', function(mapE) {
-        // Define 'mapE' as a Global variable
-        mapEvent = mapE;
-
-        // Show form
-        form.classList.remove('hidden');
-
-        // Make focus on 'input' field
-        inputDistance.focus();
+  _getPosition() {
+    // Get position(Geolocation API):
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function() {
+        alert('Could not get your position')
       });
-    },
-    // Error
-    function() {
-      alert('Could not get your position')
-    }
-  );
+  }
 
-  // Form submit
-  form.addEventListener('submit', function(e) {
+  _loadMap(position) {
+    // Get coords
+    const { latitude, longitude } = position.coords;
+    console.log(`https://www.google.com.ua/maps/@${latitude},${longitude},13z`);
+
+    const coords = [latitude, longitude];
+    const staticCoords = [50.3671577, 30.4406045];
+
+    console.log(this);
+
+    this.#map = L.map('map').setView(coords, 13);
+    console.log(this.#map);
+
+    // Google Map tiles
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 18,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(this.#map);
+
+    // Handling Clicks on Map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    // Show form
+    form.classList.remove('hidden');
+    // Make focus on 'input' field
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    // Select 'parent elem' of type/toggle change
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
     e.preventDefault();
-
     // Clear form's inputs
     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''; 
-
     // Display marker
-    console.log(mapEvent);
-    const { lat, lng } = mapEvent.latlng;
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
 
     // Create marker
     L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(L.popup({
-      maxWidth: 250, 
-      maxWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup'
-    }))
+    .addTo(this.#map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250, 
+        maxWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      })
+    )
     .setPopupContent('Workout')
     .openPopup();
-  });
-};
+  }
+}
 
-inputType.addEventListener('change', function() {
-  // Select 'parent elem' of type/toggle change
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
